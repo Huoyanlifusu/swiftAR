12982b01bb4a8f696a6afc07569d281931e3ac40
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-02-17 22:29:24 +0800

    Add files via upload

--- 练习.playground/Contents.swift
+++ 练习.playground/Contents.swift
@@ -1,19 +1,43 @@
 //: A UIKit based Playground for presenting user interface
 import Foundation
 
-// 运算符重载
 struct Point {
-    var x: Int
-    var y: Int
+    var x: Double
+    var y: Double
     
+    
+    // 运算符重载
     static func + (p1: Point, p2: Point) -> Point {
         return Point(x: p1.x + p2.x, y: p1.y + p2.y)
     }
+    
+    
+    //swift 下标
+    subscript(index: Int) -> Double {
+        set {
+            if index == 0 {
+                x = newValue
+            } else {
+                y = newValue
+            }
+        }
+        get {
+            if index == 0 {
+                return x
+            }
+            if index == 1 {
+                return y
+            }
+            return 0
+        }
+    }
+    
 }
 
-let p1 = Point(x: 5, y: 5)
+let p1 = Point(x: 5.2, y: 5.5)
 let p2 = Point(x: 8, y: 8)
 
-print("\(p1+p2)")
+//print("\(p1+p2)")
 
 //
+print("\(p1.x)")
