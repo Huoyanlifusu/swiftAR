fe2949be7347fc1cf32e20c42645ccadebf418de
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-02-19 04:18:16 +0800

    Add files via upload

--- 练习代码/练习代码/main.m
+++ 练习代码/练习代码/main.m
@@ -35,18 +35,70 @@
 @end
 
 
+@interface Calculator : NSObject
+//累加方法
+-(void) setAccumulator: (int) a;
+-(void) clear;
+-(double) accumulator;
+//算术方法
+-(void) add: (double) value;
+-(void) subtract: (double) value;
+-(void) multiply: (double) value;
+-(void) divide: (double) value;
+@end
+
+@implementation Calculator
+{
+    double accumulator;
+}
+-(void) setAccumulator: (int) a{
+    accumulator = a;
+}
+-(void) clear {
+    accumulator = 0;
+}
+-(double) accumulator {
+    return accumulator;
+}
+-(void) add: (double) value {
+    accumulator += value;
+}
+-(void) subtract: (double) value {
+    accumulator -= value;
+}
+-(void) multiply: (double) value {
+    accumulator *= value;
+}
+-(void) divide: (double) value {
+    accumulator /= value;
+}
+
+@end
+
+
 int main(int argc, const char *argv[]) {
     //第一个示例程序
     @autoreleasepool {
-        Fraction *myFraction;
-        myFraction = [Fraction alloc];
-        myFraction = [myFraction init];
+        Calculator *myCalculator = [[Calculator alloc] init];
+        [myCalculator setAccumulator: 100];
+        [myCalculator add: 30.0];
+        NSLog(@"The result is %g", [myCalculator accumulator]);
         
-        [myFraction setNumerator: 1];
-        [myFraction setDenominator: 3];
         
-        NSLog(@"the value of my fraction is:");
-        [myFraction print];
+        
+        
+//        Fraction *myFraction;
+//        myFraction = [Fraction alloc];
+//        myFraction = [myFraction init];
+//
+//        [myFraction setNumerator: 1];
+//        [myFraction setDenominator: 3];
+//
+//        NSLog(@"the value of my fraction is:");
+//        [myFraction print];
+        
+        
+        
 //        int result, value1, value2;
 //        value1 = 87;
 //        value2 = 15;
