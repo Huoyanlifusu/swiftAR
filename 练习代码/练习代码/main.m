102824f137b09c7d7e1dc7a0fd060231054b6bb8
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-02-19 03:51:24 +0800

    Add files via upload

--- 练习代码/练习代码/main.m
+++ 练习代码/练习代码/main.m
@@ -7,13 +7,51 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
+@interface Fraction : NSObject
+-(void) print;
+-(void) setNumerator: (int) n;
+-(void) setDenominator: (int) d;
+@end
+
+
+
+@implementation Fraction
+{
+    int numerator;
+    int denominator;
+}
+
+-(void) print {
+    NSLog(@"%i/%i", numerator, denominator);
+}
+
+-(void) setNumerator: (int) n {
+    numerator = n;
+}
+
+-(void) setDenominator: (int) d {
+    denominator = d;
+}
+@end
+
+
+int main(int argc, const char *argv[]) {
+    //第一个示例程序
     @autoreleasepool {
-        int result, value1, value2;
-        value1 = 87;
-        value2 = 15;
-        result = value1 - value2;
-        NSLog(@"the subtraction of %i and %i is %i", value1, value2, result);
+        Fraction *myFraction;
+        myFraction = [Fraction alloc];
+        myFraction = [myFraction init];
+        
+        [myFraction setNumerator: 1];
+        [myFraction setDenominator: 3];
+        
+        NSLog(@"the value of my fraction is:");
+        [myFraction print];
+//        int result, value1, value2;
+//        value1 = 87;
+//        value2 = 15;
+//        result = value1 - value2;
+//        NSLog(@"the subtraction of %i and %i is %i", value1, value2, result);
     }
     return 0;
 }
