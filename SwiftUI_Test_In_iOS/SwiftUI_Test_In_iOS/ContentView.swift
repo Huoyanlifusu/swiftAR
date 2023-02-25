727da6c22a10809a425a9d9fa138be8b703f9c2e
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-02-25 15:29:54 +0800

    swiftUI学习

---
+++ SwiftUI_Test_In_iOS/SwiftUI_Test_In_iOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI_Test_In_iOS
+//
+//  Created by 张裕阳 on 2023/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Test_In_iOS/SwiftUI_Test_In_iOS/SwiftUI_Test_In_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Test_In_iOSApp.swift
+//  SwiftUI_Test_In_iOS
+//
+//  Created by 张裕阳 on 2023/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Test_In_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
